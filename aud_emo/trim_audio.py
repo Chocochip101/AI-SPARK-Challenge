5fd9acc5498af63a012f2ed59c256321cfd11f8c
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-04-02 15:14:38 +0900

    Update trim_audio.py

--- aud_emo/trim_audio.py
+++ aud_emo/trim_audio.py
@@ -1,12 +1,15 @@
-# author: Moongi & Juhui
-# edit: Jeiyoon
 import os
 from pydub import AudioSegment
 import moviepy.editor as mp
 import math
 
-video_dir = "/root/clip/video_datasets/SumMe/SumMe/videos/"
-audio_dir = "/root/clip/video_datasets/SumMe/SumMe/video_to_audio/"
+# tvsum: "/root/clip/video_datasets/tvsum50_ver_1_1/ydata-tvsum50-v1_1/ydata-tvsum50-video/video/"
+# summe: "/root/clip/video_datasets/SumMe/SumMe/videos/"
+video_dir = "/root/clip/video_datasets/tvsum50_ver_1_1/ydata-tvsum50-v1_1/ydata-tvsum50-video/video/"
+
+# tvsum: "/root/clip/video_datasets/tvsum50_ver_1_1/ydata-tvsum50-v1_1/ydata-tvsum50-video/video_to_audio/"
+# summe: "/root/clip/video_datasets/SumMe/SumMe/video_to_audio/"
+audio_dir = "/root/clip/video_datasets/tvsum50_ver_1_1/ydata-tvsum50-v1_1/ydata-tvsum50-video/video_to_audio/"
 wave_cut_dir = "/root/clip/video_datasets/wav_cut/"
 
 mp4_name_list = os.listdir(video_dir)
@@ -15,13 +18,13 @@ mp4_name_list = os.listdir(video_dir)
 for mp4_name in mp4_name_list:
     if mp4_name.find('mp4') is not -1: # **.webm / **.mp4
         name = mp4_name[:-4]
-        video_clip = mp.VideoFileClip(str(video_dir + name + ".mp4"))
+        video_clip = mp.VideoFileClip(video_dir + name + ".mp4")
 
         # no audio signal
         if video_clip.audio == None:
             continue
 
-        video_clip.audio.write_audiofile(str(audio_dir + name + ".mp3"))
+        video_clip.audio.write_audiofile(audio_dir + name + ".mp3")
 
         src = audio_dir + name + ".mp3"
         dst = audio_dir + name + ".wav"
@@ -30,10 +33,14 @@ for mp4_name in mp4_name_list:
 
         sound2 = AudioSegment.from_mp3(dst)
 
-        two_seconds = 1 * 1000 # ms
+        # two_seconds = 1 * 1000 # ms
+        two_seconds = 1 * 1000 * 4 # ms
 
         # math.floor(): 실수를 내림하여 정수 반환
         # e.g.) len(sound2) = 169340
-        for i in range(int(math.floor(len(sound2) / 1000))): # 169
+        """
+        Disclaimer: for문에 길이 꼭 체크!!! (e.g. 1000 or 4000)
+        """
+        for i in range(int(math.floor(len(sound2) / 4000))): # 169
             slice = sound2[i * two_seconds : (i + 1) * two_seconds]
             slice.export(wave_cut_dir + name + '_{}.wav'.format(i), format="wav")
