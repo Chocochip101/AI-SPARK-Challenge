7662a9bff2547a48e2b114bd59a2d792415f0548
k4ke <56618962+Jeiyoon@users.noreply.github.com>
2022-04-04 17:39:43 +0900

    Update inference_binary_emotion.py

--- aud_emo/inference_binary_emotion.py
+++ aud_emo/inference_binary_emotion.py
@@ -8,6 +8,8 @@ import pandas as pd
 
 from dataclasses import dataclass
 from typing import Optional, Tuple
+from collections import defaultdict, Counter
+import matplotlib.pyplot as plt
 
 import librosa
 
@@ -24,6 +26,7 @@ from transformers.models.wav2vec2.modeling_wav2vec2 import (
     Wav2Vec2PreTrainedModel,
     Wav2Vec2Model
 )
+
 """
 (1) normalize audio
 """
@@ -266,7 +269,7 @@ def prediction(df_row):
     # [{'Emotion': 'emotion', 'Score': '0.1%'}, {'Emotion': 'neutral', 'Score': '99.9%'}]
     # print("outputs: ", outputs)
     if float(outputs[0]['Score'].replace("%", "")) > float(outputs[1]['Score'].replace("%", "")): # emotion
-        return 1
+        return 5
     else: # neutral
         return 0
 
@@ -282,22 +285,86 @@ test_tv = test_tv.sort_values(by='index' ,ascending = True)
 
 emotion_per_second = []
 
-# print(prediction(test_tv.iloc[0]))
-
-from collections import Counter
-import matplotlib.pyplot as plt
-
 for idx in tqdm(range(len(test_tv['name']))):
-    # four seconds
+    # four seconds / mean
+    # emotion_per_second.append(prediction(test_tv.iloc[idx]))
+    emotion_per_second.append(0)
     emotion_per_second.append(prediction(test_tv.iloc[idx]))
     emotion_per_second.append(prediction(test_tv.iloc[idx]))
-    emotion_per_second.append(prediction(test_tv.iloc[idx]))
-    emotion_per_second.append(prediction(test_tv.iloc[idx]))
+    emotion_per_second.append(0)
+    # emotion_per_second.append(prediction(test_tv.iloc[idx]))
 
 print(Counter(emotion_per_second))
+print("emotion_per_second: ", emotion_per_second)
+
+"""
+importance score
+"""
+wav_cut_path = "/root/clip/video_datasets/normed_wav_cut"
+tvsum_anno_path = "/root/clip/ydata-tvsum50-anno.tsv"
+tvsum_info_path = "/root/clip/ydata-tvsum50-info.tsv"
+
+df = pd.read_csv(tvsum_anno_path, sep='\t', names=["video_name", "task", "importance_score"])
+print(df)
+
+# 'video_id', 'length'
+df_info = pd.read_csv(tvsum_info_path, sep='\t')
+video_info_dict = defaultdict()
+# print(df_info['video_id'])
+# print(df_info['length'])
+
+for idx in range(len(df_info['video_id'])):
+    video_id = df_info['video_id'][idx]
+    video_length = df_info['length'][idx].split(":")
+    video_length = int(video_length[0]) * 60 + int(video_length[1])
+
+    video_info_dict[video_id] = video_length
+
+print(video_info_dict)
+
+# check fps: all the tvsum video fps are not equal
+score_per_second = []
+avg_importance_score = defaultdict()
+video_counter = Counter(df['video_name'])
+
+for idx in tqdm(range(len(df['video_name']))): # 1000
+    print("idx: ", idx)
+    print("df['video_name'][idx]: ", df['video_name'][idx])
+    # print("df['task'][idx]: ", df['task'][idx])
+
+    importance_score = df['importance_score'][idx].split(',')
+
+    for fps in range(1, len(importance_score) + 1):
+        tvsum_fps = round((len(importance_score) / video_info_dict[str(df['video_name'][idx])]) * 2)
+
+        if fps % tvsum_fps == 0:
+            # the TVSum dataset provides human annotated importance scores for every two second of each video
+            score_per_second.append(importance_score[fps])
+            score_per_second.append(importance_score[fps])
+
+
+    if str(df['video_name'][idx]) in avg_importance_score:
+        avg_importance_score[str(df['video_name'][idx])] += np.float32(np.array(score_per_second))
+    else:
+        avg_importance_score[str(df['video_name'][idx])] = np.float32(np.array(score_per_second))
+
+    # print("len(score_per_second) :", len(score_per_second))
+    score_per_second.clear()
+
+    if idx == 19:
+        break
+
+# mean
+# AwmHb44_ouw
+for v_name in avg_importance_score:
+    avg_importance_score[v_name] = avg_importance_score[v_name] / video_counter[v_name]
+
 
 x = np.arange(0, len(emotion_per_second))
-plt.plot(x, emotion_per_second, color = 'red')
+plt.figure(figsize=(18, 6))
+plt.bar(x, emotion_per_second, color = 'peachpuff', alpha = 0.9)
+plt.plot(x, avg_importance_score["AwmHb44_ouw"], color = 'firebrick') #
+plt.legend(['importance score', 'emotion'])
 plt.xlabel("Times (s)")
-plt.ylabel("Emotion")
+plt.ylabel("Importance Score")
 plt.show()
